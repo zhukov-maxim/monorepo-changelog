917db6ea48db758af5c74f5288e28cc8b3d6682b
Жуков Максим Александрович <MAZhukov.SBT@sberbank.ru>
2017-05-05 14:36:37 +0700

    Refactor index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const walkDir = require('./io');
 
 console.log('Start:\n');
 
-const startDate = '2017-04-20';
+const START_DATE = '2017-04-22';
 
-const getNewPartOfChangelog = (changelog, previousDate) => {
-  const sampleDates = changelog.match(dateRegex);
-  const datesBeforeStart = sampleDates.filter(element => element <= previousDate);
+// Get the part of a changelog not older than start date.
+const getNewPartOfChangelog = (changelog, startDate) => {
+  const datesAll = changelog.match(dateRegex);
+  const datesBeforeStart = datesAll.filter(element => element < startDate);
   const previousReleaseDate = datesBeforeStart[0];
 
   const previousReleaseDateIndex = changelog.indexOf(previousReleaseDate);
@@ -18,34 +19,51 @@ const getNewPartOfChangelog = (changelog, previousDate) => {
   return newPart;
 };
 
+// Remove word 'changelog' from changelog title.
+const stripWordChangelog = (changelog) => {
+  const removedPart = ' changelog';
+  const firstLine = changelog.split('\n')[0];
+  const lastIndex = firstLine.lastIndexOf(removedPart);
+
+  const beforeRemovedPart = changelog.substring(0, lastIndex);
+  const afterRemovedPart = changelog.substring(lastIndex + removedPart.length);
+
+  const strippedChangelog = beforeRemovedPart.concat(afterRemovedPart);
+
+  return strippedChangelog;
+};
+
 const currentDir = __dirname;
 const changelogsDir = `${currentDir}/../markup`;
 
+// Read all files except for junk and files inside node_modules.
 const allFiles = walkDir(changelogsDir, true, true);
-const changelogFiles = allFiles.filter((element) => {
-  if (element.includes('CHANGELOG.md')) {
-    return true;
-  }
-  return false;
-});
 
+// Read all files except for junk and files inside node_modules.
+const changelogFiles = allFiles.filter(element => element.endsWith('CHANGELOG.md'));
+
+// Get content of all changelog files.
 const changelogs = changelogFiles.map(element => fs.readFileSync(element, 'UTF-8'));
 
-const newChangelogs = [];
-changelogs.forEach((element) => {
-  const newPart = getNewPartOfChangelog(element, startDate);
+const updatedChangelogs = [];
+
+// Get parts of changelogs updated since START_DATE.
+// Start date is included.
+changelogs.forEach((changelog) => {
+  const newPart = getNewPartOfChangelog(changelog, START_DATE);
   const hasUpdate = newPart.match(dateRegex);
 
   if (hasUpdate) {
-    newChangelogs.push(newPart);
+    const formattedNewPart = stripWordChangelog(newPart);
+    updatedChangelogs.push(formattedNewPart);
   }
 });
 
-const newChangelogsList = newChangelogs.reduce((list, element) => list + element).trim();
+const updatedChangelogsList = updatedChangelogs.reduce((list, element) => list + element).trim();
 
-console.log(newChangelogsList);
+console.log(updatedChangelogsList);
 
-const ouputFile = `${currentDir}/SUMMARY after ${startDate}.md`;
-fs.writeFileSync(ouputFile, newChangelogsList, 'UTF-8');
+const ouputFile = `${currentDir}/SUMMARY after ${START_DATE}.md`;
+fs.writeFileSync(ouputFile, updatedChangelogsList, 'UTF-8');
 
 console.log('\nEnd.');
