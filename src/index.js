e16f6a12bbef82a979f8fb2808142bfb3adca3a4
Maxim Zhukov <zhkv.mxm@gmail.com>
2017-07-01 19:21:38 +0700

    Implement ability to specify end date

--- src/index.js
+++ src/index.js
@@ -11,9 +11,13 @@ const rawDefaultStartDate = new Date();
 rawDefaultStartDate.setDate(rawDefaultStartDate.getDate() - DEFAULT_NUMBER_OF_LAST_DAYS);
 const formattedDefaultStartDate = utils.formatDate(rawDefaultStartDate);
 
+const rawDefaultEndDate = new Date();
+const formattedDefaultEndDate = utils.formatDate(rawDefaultEndDate);
+
 const defaultOptions = {
   monoRepoPath: __dirname,
-  startDate: formattedDefaultStartDate
+  startDate: formattedDefaultStartDate,
+  endDate: formattedDefaultEndDate
 };
 
 const getMonorepoChangelog = (userOptions) => {
@@ -23,7 +27,7 @@ const getMonorepoChangelog = (userOptions) => {
     throw new Error('Monorepo path doesn\'t exist.');
   }
 
-  const { monoRepoPath, startDate } = options;
+  const { monoRepoPath, startDate, endDate } = options;
 
   // Read all files except for junk and files inside node_modules.
   const allFiles = io.walkDir(monoRepoPath, true, true);
@@ -35,17 +39,9 @@ const getMonorepoChangelog = (userOptions) => {
   const changelogs = changelogFiles.map(element => fs.readFileSync(element, 'utf8'));
 
   // Get new parts of updated changelogs.
-  const updatedChangelogs = utils.getUpdatedChangelogs(changelogs, startDate);
+  const updatedChangelogs = utils.getUpdatedChangelogs(changelogs, startDate, endDate);
 
-  if (!updatedChangelogs.length) {
-    return '';
-  }
-
-  // Concatenated list of changelogs.
-  const updatedChangelogsList = updatedChangelogs.reduce((list, element) =>
-    list + element).trim();
-
-  return updatedChangelogsList;
+  return updatedChangelogs;
 };
 
 module.exports = getMonorepoChangelog;
--- src/utils.js
+++ src/utils.js
@@ -13,48 +13,39 @@ const utils = {
     return `${year}-${month}-${day}`;
   },
 
-  // Get parts of changelogs updated since START_DATE.
-  // Start date is included.
-  getUpdatedChangelogs: (changelogs, startDate) => {
+  // Get parts of changelogs updated between startDate and date inclusively.
+  getUpdatedChangelogs: (changelogs, startDate, endDate) => {
     const updatedChangelogs = [];
 
     changelogs.forEach((changelog) => {
-      const newPart = utils.getNewPartOfChangelog(changelog, startDate);
-      const hasUpdate = newPart.match(dateRegex);
+      const changelogPart = utils.getChangelogPart(changelog, startDate, endDate);
+      const hasUpdate = changelogPart.match(dateRegex);
 
       if (hasUpdate) {
-        const formattedNewPart = utils.stripWordChangelog(newPart);
-        updatedChangelogs.push(formattedNewPart);
+        updatedChangelogs.push(changelogPart);
       }
     });
 
-    return updatedChangelogs;
+    return updatedChangelogs.join('\n\n\n');
   },
 
-  // Get the part of a changelog not older than start date.
-  getNewPartOfChangelog: (changelog, startDate) => {
-    const datesAll = changelog.match(dateRegex);
-    const datesBeforeStart = datesAll.filter(element => element < startDate);
-    const previousReleaseDate = datesBeforeStart[0];
+  // Get the part of a changelog between startDate and date inclusively.
+  getChangelogPart: (changelog, startDate, endDate) => {
+    const titleLine = changelog.split('\n')[0];
+    const indexOfWordChangelog = titleLine.indexOf(' changelog');
+    const title = titleLine.substring(0, indexOfWordChangelog);
 
-    const previousReleaseDateIndex = changelog.indexOf(previousReleaseDate);
-    const lastIndex = changelog.lastIndexOf('##', previousReleaseDateIndex);
-    const newPart = changelog.substring(0, lastIndex).trim().concat('\n\n\n');
+    const indexOfLatestRelease = changelog.indexOf('## ') + 3;
+    const allReleases = changelog.substring(indexOfLatestRelease);
+    const allReleasesArray = allReleases.split('\n\n\n## ');
 
-    return newPart;
-  },
+    const releasesBetweenDatesArray = allReleasesArray.filter((release) => {
+      const releaseDate = release.match(dateRegex);
 
-  // Remove word 'changelog' from changelog title.
-  stripWordChangelog: (changelog) => {
-    const removedPart = ' changelog';
-    const firstLine = changelog.split('\n')[0];
-    const lastIndex = firstLine.lastIndexOf(removedPart);
+      return releaseDate >= startDate && releaseDate <= endDate;
+    });
 
-    const beforeRemovedPart = changelog.substring(0, lastIndex);
-    const afterRemovedPart = changelog.substring(lastIndex + removedPart.length);
-    const strippedChangelog = beforeRemovedPart.concat(afterRemovedPart);
-
-    return strippedChangelog;
+    return `${title}\n\n## ${releasesBetweenDatesArray.join('\n\n\n## ')}`;
   }
 };
 
